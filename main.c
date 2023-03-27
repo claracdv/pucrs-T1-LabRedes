2703b01584a525bd8e64f7ecae069e44fe1df542
Clara D'Ávila <claracunhadavila@gmail.com>
2023-03-27 00:11:47 -0300

    commit

--- main.c
+++ main.c
@@ -453,64 +453,31 @@ int loop()
 
 	return 0;
 }
-
-// MAIN DA PROF
-// int main(int argc, char *argv[])
-//{
-/* Criacao do socket. Todos os pacotes devem ser construidos a partir do protocolo Ethernet. */
-/* De um "man" para ver os parametros.*/
-/* htons: converte um short (2-byte) integer para standard network byte order. */
-// if ((sockd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) < 0)
-//{
-//	printf("Erro na criacao do socket.\n");
-//	exit(1);
-// }
-
-// O procedimento abaixo eh utilizado para "setar" a interface em modo promiscuo
-// strcpy(ifr.ifr_name, "eth0");
-// if (ioctl(sockd, SIOCGIFINDEX, &ifr) < 0)
-//	printf("erro no ioctl!");
-// ioctl(sockd, SIOCGIFFLAGS, &ifr);
-// ifr.ifr_flags |= IFF_PROMISC;
-// ioctl(sockd, SIOCSIFFLAGS, &ifr);
-
-// recepcao de pacotes
-// while (1)
-//{
-//	recv(sockd, (char *)&buff1, sizeof(buff1), 0x0);
-//	// impress�o do conteudo - exemplo Endereco Destino e Endereco Origem
-//	printf("MAC Destino: %x:%x:%x:%x:%x:%x \n", buff1[0], buff1[1], buff1[2], buff1[3], buff1[4], buff1[5]);
-//	printf("MAC Origem:  %x:%x:%x:%x:%x:%x \n\n", buff1[6], buff1[7], buff1[8], buff1[9], buff1[10], buff1[11]);
-//}
-
 int main(int argc, char *argv[])
 {
-	/* Criacao do socket. Todos os pacotes devem ser construidos a partir do protocolo Ethernet. */
-	/* De um "man" para ver os parametros.*/
-	/* htons: converte um short (2-byte) integer para standard network byte order. */
-	if ((sockd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) < 0)
-	{
-		printf("Erro na criacao do socket.\n");
-		exit(1);
-	}
+    /* Criacao do socket. Todos os pacotes devem ser construidos a partir do protocolo Ethernet. */
+    /* De um "man" para ver os parametros.*/
+    /* htons: converte um short (2-byte) integer para standard network byte order. */
+    if ((sockd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) < 0)
+    {
+        printf("Erro na criacao do socket.\n");
+        exit(1);
+    }
 
-	char interface[10];
-	printf("Set interface mode: ");
-	scanf("%s", interface);
+    // O procedimento abaixo eh utilizado para "setar" a interface em modo promiscuo
+    strcpy(ifr.ifr_name, "eth0");
+    if (ioctl(sockd, SIOCGIFINDEX, &ifr) < 0)
+        printf("erro no ioctl!");
+    ioctl(sockd, SIOCGIFFLAGS, &ifr);
+    ifr.ifr_flags |= IFF_PROMISC;
+    ioctl(sockd, SIOCSIFFLAGS, &ifr);
 
-	strcpy(ifr.ifr_name, interface);
-
-	if (ioctl(sockd, SIOCGIFINDEX, &ifr) < 0)
-	{
-		printf("erro no ioctl!\n");
-		printf("Favor verificar a interface selecionada!!\n");
-		return 0;
-	}
-
-	// O procedimento abaixo eh utilizado para "setar" a interface em modo promiscuo
-	ioctl(sockd, SIOCGIFFLAGS, &ifr);
-	ifr.ifr_flags |= IFF_PROMISC;
-	ioctl(sockd, SIOCSIFFLAGS, &ifr);
-
-	return loop();
+    // recepcao de pacotes
+    while (1)
+    {
+        recv(sockd, (char *)&buff1, sizeof(buff1), 0x0);
+        // impress�o do conteudo - exemplo Endereco Destino e Endereco Origem
+        printf("MAC Destino: %x:%x:%x:%x:%x:%x \n", buff1[0], buff1[1], buff1[2], buff1[3], buff1[4], buff1[5]);
+        printf("MAC Origem:  %x:%x:%x:%x:%x:%x \n\n", buff1[6], buff1[7], buff1[8], buff1[9], buff1[10], buff1[11]);
+    }
 }
